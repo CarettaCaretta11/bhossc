0aafa8fce646c66d6cd51bd3a66632d9be381773
CarettaCaretta11 <kamranmehdiyev02@gmail.com>
2022-06-20 11:42:17 +0400

    nc

--- yoyo1/settings.py
+++ yoyo1/settings.py
@@ -1,26 +1,14 @@
-import django
-django.setup()
-
 from pathlib import Path
 import os
 import cloudinary
-import cloudinary.uploader
-import cloudinary.api
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = 'di8q37btdngiodujfhiTUABGDaus7atd76ubfausdhsofnsfo87uej' # os.environ.get('SECRET_KEY')
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+DEBUG = True # os.environ.get("DEBUG", "False") == "True" 
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True #os.environ.get('DEBUG') == 'True' # os.environ.get("DEBUG", "False") == "True" 
- 
-ALLOWED_HOSTS = ['bhos.svdev.me', 'admin.svdev.me', '127.0.0.1', 'localhost', '.herokuapp.com', 'bhossc.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 # Application definition
 
@@ -58,11 +46,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'last_visit.middleware.LastVisit',
 ]
-    
-
-# CORS_ALLOWED_ORIGINS = [
-#     'http://127.0.0.1:8000/api/rooms'
-# ]
 
 CORS_ALLOW_ALL_ORIGINS = True
 
@@ -87,10 +70,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'yoyo1.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
     # 'default': { 
@@ -120,10 +99,6 @@ DATABASES = {
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -139,70 +114,35 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/3.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'Asia/Baku'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
-
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-
 STATIC_URL = '/static/'
-
 MEDIA_URL = '/images/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+MEDIA_ROOT = BASE_DIR / 'static/images'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-MEDIA_ROOT = BASE_DIR / 'static/images'
-
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_BACKEND = 'email_backend.DKIMBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_FILE_PATH = str(BASE_DIR.joinpath('sent_emails'))
 EMAIL_HOST = 'smtp.mail.ru'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_USER =  os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-
 EMAIL_USE_TLS = True
-# EMAIL_HOST = 'smtp.gmail.com'  # Gmail’s SMTP server == domain name
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = "pltrfstd@gmail.com"
-# EMAIL_HOST_PASSWORD = "topcxyrwqvlyqhzh"
 
 ASGI_APPLICATION = "yoyo1.asgi.application"
-
-# DO NOT UNCOMMENT CODE COMMENTED BELOW
-# CHANNEL_LAYERS = {
-#     'default': {
-#         'BACKEND': 'channels_redis.core.RedisChannelLayer',
-#         'CONFIG': {
-#             "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
-#         },
-#     },
-# }
-
 
 # CHANNEL_LAYERS = {
 #     "default": {
@@ -222,8 +162,8 @@ CHANNEL_LAYERS = {
 import django_heroku
 django_heroku.settings(locals())
 
-cloudinary.config( 
-  cloud_name = os.environ.get("CLOUDINARY_CLOUD_NAME"), 
+cloudinary.config ( 
+  cloud_name =  os.environ.get("CLOUDINARY_CLOUD_NAME"), 
   api_key = os.environ.get("CLOUDINARY_API_KEY"), 
   api_secret = os.environ.get("CLOUDINARY_API_SECRET") 
 )
