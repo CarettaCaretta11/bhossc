8b255d421e564131a7f6a4734c59b55c0e2759af
CarettaCaretta11 <kamranmehdiyev02@gmail.com>
2022-06-20 12:07:12 +0400

    nc

--- runserver_plus.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#!/usr/local/lib/python3.9
-cd /root/bhossc
-python3.9 manage.py runserver_plus --cert-file /etc/letsencrypt/live/bhos.svdev.me/cert.pem --key-file /etc/letsencrypt/live/bhos.svdev.me/privkey.pem bhos.svdev.me:8000
