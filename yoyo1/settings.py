bef30a3fc114968764d5fa90498a88a0c92ce8c3
Kamran Mekhtiyev <kamranmehdiyev02@gmail.com>
2022-06-20 21:59:55 +0400

    nc

--- yoyo1/settings.py
+++ yoyo1/settings.py
@@ -41,7 +41,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'last_visit.middleware.LastVisit',
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
